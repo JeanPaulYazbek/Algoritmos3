e409d473e79f4e5c55493ff933a8707a3da8a6fc
JeanPaulYazbek <jeanpaulyf98@hotmail.com>
2018-10-27 21:13:38 -0430

    comente , grafo, lados, arcos, vertice

--- Proyecto 1/Arco.java
+++ Proyecto 1/Arco.java
@@ -5,7 +5,14 @@ public class Arco<E> extends Lado{
 	protected Vertice vi;	// vertice inicial del arco
 	protected Vertice vf;	// vertice final del arco 
 
-	//crea arco por constructor
+	/**
+	 * crea arco por constructor
+	 * @param id id del arco a crear
+	 * @param dato dato del arco a crear 
+	 * @param p peso del arco a crear
+	 * @param vi objeto vertice inicial del arco
+	 * @param vf objeto vertice final del arco
+	 */
 	Arco(String id, E dato, double p, Vertice vi, Vertice vf){
 		this.dato = dato;
 		this.id = id;
@@ -14,32 +21,51 @@ public class Arco<E> extends Lado{
 		this.vf = vf;
 	}
 
-	//metodo que devuelve el extremo inicial de un arco
+	/**
+	 * metodo que devuelve el extremo inicial de un arco
+	 * @return objeto vertice extremo inicial del arco
+	 */
 	public Vertice getExtremoInicial(){
 		return vi;
 	}
 
-	//metodo que devuelve extremo final de un arco
+	/**
+	 * metodo que devuelve extremo final de un arco
+	 * @return objeto vertice extremo final del arco
+	 */
 	public Vertice getExtremoFinal(){
 		return vf;
 	}
 
-	//metodo que obtiene el peso de un arco
+	/**
+	 * metodo que obtiene el peso de un arco
+	 * @return peso del arco
+	 */
 	public double getPeso(){
 		return p;
 	}
     
-    //metodo que obtiene el id de un arco
+	/**
+	 * metodo que obtiene el id de un arco
+	 * @return id del arco
+	 */
 	public String getId(){
 		return id;
 	}
 
-	//metodo que obtiene el Dato de un arco
+	/**
+	 * metodo que obtiene el Dato de un arco
+	 * @return dato del arco
+	 */
 	public E getDato(){
 		return dato;
 	}
 
-	//metodo que presenta todos los atributos de un arco en forma de cadena de String
+
+	/**
+	 * metodo que presenta todos los atributos de un arco en forma de cadena de String
+	 * @return string que representa el arco
+	 */
 	public String toString(){
 		String x = id;
 		String y = String.valueOf(dato);
